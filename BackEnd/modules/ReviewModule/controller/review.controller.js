c07e282eec5566e51e5cb06951d431c811f92a52
AhmedAbdAlRahmanAnwar <ahmedabdalrahman61@gmail.com>
2022-08-03 14:51:44 +0200

    fix bug in add review end point

--- BackEnd/modules/ReviewModule/controller/review.controller.js
+++ BackEnd/modules/ReviewModule/controller/review.controller.js
@@ -5,21 +5,23 @@ const addReview = (request, response, next) => {
     const {productId, comment, rating} = request.body.payload;
     Product.findById(productId)
         .populate({path: "category", select: "categoryName -_id"})
-        .populate({path: "reviews.user", select: "firstName lastName -_id"})
         .then(product => {
             if (product) {
                 //Check if user already has a review
-                const userIndex = product.reviews.findIndex(({user})=>request.user["_id"].value === user.value)
-                if (userIndex === -1){
+                const userIndex = product.reviews
+                    .findIndex(({user}) => request.user["_id"].toString() === user.toString())
+                if (userIndex === -1) {
                     product.reviews.push({user: request.user["_id"], rating, comment});
                     product.rating = ((((product.reviews.length - 1) * parseFloat(product.rating)) + parseFloat(rating)) / product.reviews.length).toFixed(1);
                     product.save()
-                        .then(() => response.status(201).json({product}))
+                        .then(data => {
+                            data.populate({path: "reviews.user", select: "firstName lastName -_id"})
+                                .then(() => response.status(201).json({product}))
+                        })
                         .catch(error => errorHandler(error, 400, next))
-                }else{
+                } else {
                     errorHandler("Only One Review Allowed per product", 400, next);
                 }
-
             } else {
                 errorHandler("Product Not Found", 404, next)
             }
