609a4af0096e4d9b7c493e961e4eed63aee25781
AhmedAbdAlRahmanAnwar <ahmedabdalrahman61@gmail.com>
2022-08-10 20:03:15 +0200

    added update address endpoint

--- BackEnd/Validators/addressValidator.js
+++ BackEnd/Validators/addressValidator.js
@@ -1,6 +1,7 @@
 const {body} = require('express-validator');
 
 module.exports = [
+    body("payload").exists().withMessage("No address data provided"),
     body("payload.street").matches(/^[a-zA-Z0-9 ,]{3,}$/).withMessage("street should be a string"),
     body("payload.city").matches(/^[a-zA-Z]{3,}$/).withMessage("city should be a string"),
     body("payload.country").matches(/^[a-zA-Z]{3,}$/).withMessage("country should be a string"),
--- BackEnd/modules/UserModule/controller/user.address.controller.js
+++ BackEnd/modules/UserModule/controller/user.address.controller.js
@@ -1,5 +1,6 @@
 const User = require('./../model/user.model');
 const errorHandler = require('./../../../Utilities/errorHandler');
+const {isNumeric} = require("validator");
 
 const addNewAddress = (request, response, next) => {
     const {street, city, country, mobile} = request.body.payload;
@@ -24,18 +25,50 @@ const addNewAddress = (request, response, next) => {
 }
 
 const updateAddress = (request, response, next) => {
+    const {addressId, payload} = request.body;
+    if (!addressId || !isNumeric(addressId.toString())) {
+        errorHandler("Invalid Address ID", 400, next);
+        return;
+    }
+    if (!payload) {
+        errorHandler("No Updates Provided", 400, next);
+        return;
+    }
+
+    const updates = Object.keys(request.body.payload)
+    const allowedUpdates = ["street", "city", "country", "mobile"]
+    const isValidOperation = updates.every(update => allowedUpdates.includes(update));
+    if (!isValidOperation) {
+        errorHandler("Invalid Update!", 400, next);
+        return;
+    }
+
+    User.findById(request.user["_id"])
+        .then(async user => {
+            const addressIndex = user.address.findIndex(address => address["_id"] === +addressId);
+            if (addressIndex === -1) {
+                errorHandler("Address id doesn't exist", 400, next);
+                return;
+            }
+            updates.forEach(update => {
+                user.address[addressIndex][update] = request.body.payload[update]
+            })
+            await user.save();
+            response.status(200).json({message: "address updated"});
+        })
+        .catch(error => errorHandler(error.message, 400, next))
 
 }
 
 const deleteAddress = (request, response, next) => {
     User.findById(request.user["_id"])
-        .then(async user =>{
-            const addressIndex = user.address.findIndex(address=> address["_id"] === +request.params.id);
+        .then(async user => {
+            const addressIndex = user.address.findIndex(address => address["_id"] === +request.params.id);
             if (addressIndex === -1) {
                 errorHandler("Address id doesn't exist", 400, next);
                 return;
             }
-            user.address.splice(addressIndex,1);
+            user.address.splice(addressIndex, 1);
             await user.save();
             response.status(200).json({message: "address deleted"});
         })
